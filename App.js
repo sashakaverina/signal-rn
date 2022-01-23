c00a9c8fcca1e3426048a51525a2b376028d9bf8
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-23 11:32:53 +0900

    added firebase

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Haro Sasha</Text>
     </View>
   );
 }
