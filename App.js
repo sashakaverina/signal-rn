e507900771f3d487081b350e8073e916a3b4354e
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-23 12:38:27 +0900

    login screen

--- App.js
+++ App.js
@@ -2,20 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
+const GlobalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED"},
+  headerTitleStyle: { color: "white"},
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Haro Sasha</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={GlobalScreenOptions}>
+        <Stack.Screen 
+        name='Login' component={LoginScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
