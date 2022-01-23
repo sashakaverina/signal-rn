35facdeb31a7742041bb0f3a3dbd8cdecd1bd55b
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-23 17:35:52 +0900

    added chat screen

---
+++ screens/AddChatScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const AddChatScreen = () => {
+  return (
+    <View>
+      <Text>Add Chat Screen</Text>
+    </View>
+  );
+};
+
+export default AddChatScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import CustomListItem from '../components/CustomListItem';
 import { Avatar } from 'react-native-elements';
 import { auth, db } from '../firebase';
 import { TouchableOpacity } from 'react-native';
+import {AntDesign, SimpleLineIcons} from '@expo/vector-icons'
 
 const HomeScreen = ({navigation}) => {
     const signOutUser = () => {
@@ -20,14 +21,32 @@ const HomeScreen = ({navigation}) => {
           headerStyle:  { backgroundColor: "#fff"},
           headerTitleStyle:  { color: "black"},
           headerTintColor: 'black',
-          headerLeft: () => <View style={{ marginLeft: 20}}>
+          headerLeft: () => (<View style={{ marginLeft: 20}}>
               <TouchableOpacity
               activeOpacity={0.5}
               onPress={signOutUser}>
               <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
               </TouchableOpacity>
           </View>
+          ),
+          headerRight: () => (
+              <View style={{
 
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: 80,
+                marginRight: 20,
+              }}>
+                  <TouchableOpacity activeOpacity={0.5}>
+                  <AntDesign name='camerao' size={24} color="black"/>
+                  </TouchableOpacity>
+                  <TouchableOpacity activeOpacity={0.5}
+                  onPress={() => navigation.navigate("AddChat")}>
+                  <SimpleLineIcons name='pencil' size={24} color="black"/>
+                  </TouchableOpacity>
+
+              </View>
+          ),
       });
     }, [])
   return (
