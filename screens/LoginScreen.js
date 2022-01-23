aaeec1541555a50e7edf3ab7c2a0076be287c87c
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-01-23 13:39:48 +0900

    buttons for login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Input, Image } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
+import { KeyboardAvoidingView } from 'react-native';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+const signIn = () => {
+
+};
+
   return (
-    <View>
-      <Text>I am the login screen</Text>
-    </View>
+      <KeyboardAvoidingView
+      behavior='padding' style={styles.container}>
+        <StatusBar style="light"/>
+        <Image
+        source={{
+            uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+        />
+        <View
+         style={styles.inputContainer}>
+             <Input 
+             placeholder="Email" 
+             autoFocus type="email" 
+             value={email} 
+             onChangeText={text => setEmail(text)}
+             />
+             <Input 
+             placeholder="Password" secureTextEntry type="password"
+             value={password} 
+             onChangeText={text => setPassword(text)}
+             />
+
+        </View>
+        <Button 
+        title="Login"
+        containerStyle={styles.button}
+        onPress={signIn}/>
+        <Button 
+        title="Register"
+        containerStyle={styles.button}
+        type='outline'/>
+        <View style={{ height: 100 }}/>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: 'white',
+
+
+    },
+    inputContainer: {
+    },
+    button: {
+
+    }
+})
 
 export default LoginScreen;
